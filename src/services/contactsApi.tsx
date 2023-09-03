011be7033483443f97d1c3b6f2623440efc81699
Hexact task for Junior React JS <sevan.keshish0@gmail.com>
2023-09-03 17:20:36 +0400

    m

---
+++ src/services/contactsApi.tsx
@@ -0,0 +1,38 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { Contact } from "../models/contact.models";
+
+export const contactsApi = createApi({
+  reducerPath: "contactsApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3001/" }),
+  endpoints: (builder) => ({
+    contacts: builder.query<Contact[], void>({
+      query: () => "/contacts",
+    }),
+    contact: builder.query<Contact, string>({
+      query: (id) => `/contacts/${id}`,
+    }),
+    addContact: builder.mutation<void, Contact>({
+      query: (contact) => ({
+        url: "/contacts",
+        method: "POST",
+        body: contact,
+      }),
+    }),
+    updateContact: builder.mutation<void, Contact>({
+      query: ({ id, ...rest }) => ({
+        url: `/contact/${id}`,
+        method: "PUT",
+        body: rest,
+      }),
+    }),
+    deleteContact: builder.mutation<void, string>({
+      query: (id) => ({
+        url: `/contacts/${id}`,
+        method: "DELETE",
+      }),
+    }),
+  }),
+});
+
+export const { useContactsQuery, useContactQuery, useAddContactMutation } =
+  contactsApi;
